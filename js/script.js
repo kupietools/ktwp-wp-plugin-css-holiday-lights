ab7aeaff1d8cf4774c3736fbcc8d7d30159d4d1d
Michael Kupietz <26891995+kupietools@users.noreply.github.com>
2025-12-06 01:19:06 -0500

    Fix HTML comment placement in script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
     document.addEventListener('DOMContentLoaded', 
     function() {
  
-const htmlToInsert = `<!-- Kupietools Interactive CSS Holiday Lights by Michael Kupietz. https://michaelkupietz.com/?p=12640 --><ul id="lightrope">
+const htmlToInsert = `<ul id="lightrope"><!-- Kupietools Interactive CSS Holiday Lights by Michael Kupietz. https://michaelkupietz.com/?p=12640 -->
 <li><div></div></li>
 <li><div></div></li>
 <li><div></div></li>
