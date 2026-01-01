5f7e57be39db0dbf166f1eefd13d43c2c37f43da
Michael Kupietz <26891995+kupietools@users.noreply.github.com>
2026-01-01 14:41:06 -0800

    Resync repo with local after adding js deferred enqueuing

--- ktwp-css-holiday-lights.php
+++ ktwp-css-holiday-lights.php
@@ -8,6 +8,14 @@ Author URI: https://michaelkupietz.com
 Kupietools Framework Version: 2025-aug-9
 Plugin URI: https://michaelkupietz.com/?p=12640
 Codepen demo: https://codepen.io/kupietz/pen/dPbNywN
+
+TO THE USER INSTALLING THIS PLUGIN: 
+In the /js/script.js file, the constant "htmlToInsert", which creates the HTML used to create the 
+lights, contains an HTML comment crediting the author by name and URL, visible if anyone looks at
+your site's underlying HTML code in their browser's Inspector. As I'm aware some WP performance 
+plugins remove HTML comments, you're welcome to remove that comment before using this on your site. 
+However, I'd like it if you would leave it in.
+
 */
 
 // Prevent direct access
@@ -15,7 +23,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-/* REMOVED FRAMEWORK SECTIONS 1, 2, 3 - not using for holiday lights 2025 */
+/* REMOVED KUPIETOOLS FRAMEWORK SECTIONS 1, 2, 3 - not using for holiday lights 2025 */
  
 /* * * * * DON'T MESS WITH ANYTHING BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING * * * * */
 
@@ -25,14 +33,14 @@ function ktwp_css_holiday_lights_enqueue_css() {
 add_action('wp_enqueue_scripts', 'ktwp_css_holiday_lights_enqueue_css');
 
 function ktwp_css_holiday_lights_enqueue_assets() {
-    wp_enqueue_script('ktwp_css_holiday_lights-script', plugins_url('js/script.js', __FILE__), array('jquery'), '1.0',  array('in_footer' => true,'strategy'  => 'defer') );
+    wp_enqueue_script('ktwp_css_holiday_lights-script', plugins_url('js/script.js', __FILE__), array('jquery'), '1.0', array('in_footer' => true,'strategy'  => 'defer'));
 }
 
 add_action('wp_enqueue_scripts', 'ktwp_css_holiday_lights_enqueue_assets');
 
-/* NOTE: If you really wanted to completely avoid javascript, replace the above by checking for the 
-   wp_body_open hook and using that if it's available, and falling back to javascript for the small 
-   percentage of WP sites on WP or theme versions that don't support it: 
+/* NOTE: If you really wanted to completely avoid javascript, replace the above add_action by checking 
+   for the wp_body_open hook and using that if it's available, and falling back to javascript for the 
+   small percentage of WP sites on WP or theme versions that don't support it: 
  
  <?php
 function insert_lightrope_html() {
